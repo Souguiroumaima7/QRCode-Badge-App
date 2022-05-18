d296dfe651af5b75287b3c5c2e74e58e6831ca14
Souguir Oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2022-05-18 17:20:23 +0100

    without modification

---
+++ QRCodeBadge/src/main/kotlin/com/example/QRCodeBadge/QrCodeBadgeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.QRCodeBadge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QrCodeBadgeApplication
+
+fun main(args: Array<String>) {
+	runApplication<QrCodeBadgeApplication>(*args)
+}
---
+++ QRCodeBadge/src/test/kotlin/com/example/QRCodeBadge/QrCodeBadgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.QRCodeBadge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QrCodeBadgeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
