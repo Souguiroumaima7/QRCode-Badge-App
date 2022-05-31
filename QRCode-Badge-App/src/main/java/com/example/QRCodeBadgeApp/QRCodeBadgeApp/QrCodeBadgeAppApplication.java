77871f9a873233a818821a7e68acc3910f159b09
Souguir Oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2022-05-31 17:44:25 +0100

    Add files via upload

---
+++ QRCode-Badge-App/src/main/java/com/example/QRCodeBadgeApp/QRCodeBadgeApp/QrCodeBadgeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.QRCodeBadgeApp.QRCodeBadgeApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrCodeBadgeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrCodeBadgeAppApplication.class, args);
+	}
+
+}
